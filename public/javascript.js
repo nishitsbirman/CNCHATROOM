df46938183a68ea8d5f98e3a54a59e84623555df
nishitsbirman <90794507+nishitsbirman@users.noreply.github.com>
2021-10-08 23:26:11 +0530

    Update javascript.js

--- public/javascript.js
+++ public/javascript.js
@@ -16,12 +16,11 @@ if (!user) {
 
 var socket = io();
 
-// The user count. Can change when someone joins/leaves
+// The user count. Can change when someone joins
 socket.on('count', function (data) {
   $('.user-count').html(data);
 });
 
-// When we receive a message
 // it will be like { user: 'username', message: 'text' }
 socket.on('message', function (data) {
   $('.chat').append('<p><strong>' + data.user + '</strong>: ' + data.message + '</p>');
